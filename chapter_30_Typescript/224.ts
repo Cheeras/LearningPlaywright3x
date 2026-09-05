c2041d565c77d5d4de3c08c50f945fae226f04e0
Cheeras <shankarcheerala@gmail.com>
2026-09-05 08:37:06 +0530

    Rename function in 224.ts from sayHello to sayHello2

--- chapter_30_Typescript/224.ts
+++ chapter_30_Typescript/224.ts
@@ -1,7 +1,7 @@
 console.log("Hi, Welcome to TypeScript");
 
-function sayHello(message: string):void {
+function sayHello2(message: string):void {
     console.log("Hello,How are you " + message);
 } 
 
-sayHello("John");
+sayHello2("John");
